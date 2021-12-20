eaad245c7e4c30ceb0643df24f1a2233ac42c528
cosme <cosme.chavez@advancedhpc.com>
2021-12-20 14:25:06 -0500

    deleted pyc files

--- src/machine/pacman.py
+++ src/machine/pacman.py
@@ -29,7 +29,7 @@ class Pacman():
         def manager(self, value):
                 self._manager = value
         
-        # For the programs, there has to be an object define, leave blank for now.
+        # For the programs, there has to be a consice object. No time for that, leave blank for now.
         
 
         # ---- OS Queries ----
@@ -41,14 +41,26 @@ class Pacman():
                         return False
 
         
-        def check_manager(self):
+        def check_manager(self): #Note, this might be wrong. Possibly test in the future on other distros.
                 result = ""
                 if "CentOS" in self._distro[0]:
                         result = "yum"
+                elif "Ubuntu" in self.distro[0]:
+                        result = "apt"
+                elif "debian" in self.distro[0]:
+                        result = "apt"
+                elif "arch" in self.distro[0]:
+                        result = "pacman"
+                elif "redhat" in self.distro[0]:
+                        result = "yum"
                 else:
                         exit("Unsupported Linux distribution.")
-                print(result)
+                result = self.check_installed(result)
                 self._manager = result
+
+        def install_program(self, program):
+                try:
+                        program = Run.return_all(self._manager + " " + program) #should be yum blah
 
 
 
@@ -58,7 +70,7 @@ class Pacman():
                         exit("You need to have root privilages to run this program. \nPlease try again using sudo. Exiting.")
                         
                 # Check the package manager
-                manager = self.check_manager()
+                self.check_manager()
 
                 # Check necessary packages are installed, if not install them
                 fio = self.check_installed("fio")
